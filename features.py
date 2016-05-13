afde8ff493a4294cc67b8cabcd51de82f9c6ff6b
Chase Basich <chasebasich@gmail.com>
2016-05-13 02:29:19 -0700

    All cardinal directions seem to be working

--- features.py
+++ features.py
@@ -19,32 +19,41 @@ def main():
             img, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
   scaled = cv2.resize(im_bw, (48, 48))
 
-
   CARDINAL_DIRS = [
-    # startcol, startrow, drow, dcol
-    (0, 0, 1, 0)
+    # startcol, startrow, drow, dcol, row_order
+    (0, 0, 1, 1, 1),
+    (0, 0, 1, 1, 0),
+    (47, 0, -1, 1, 1),
+    (0, 47, 1, -1, 0)
   ]
-  full_row_vals = []
-  for i in range(48):
-    # each row
-    l_i = 0
-    layers = [0] * 3
-    start = None
-    for j in range(48):
-      if start == None and scaled[i,j] == 0:
-        start = j
-      if start != None and scaled[i,j] != 0:
-        layers[l_i] = j-start
-        l_i += 1
-        start = None
-        if l_i == 3:
-          break
-    full_row_vals.append(layers)
+  directionLayers = []
+  for d in CARDINAL_DIRS:
+    full_row_vals = []
+    for i in range(48):
+      # each row
+      l_i = 0
+      layers = [0] * 3
+      start = None
+      for j in range(48):
+        if d[4] == 1:
+          pixelVal = scaled[d[0] + i * d[2], d[1] + j * d[3]];
+        else:
+          pixelVal = scaled[d[1] + j * d[3], d[0] + i * d[2]];
+        if start == None and pixelVal == 0:
+          start = j
+        if start != None and pixelVal != 0:
+          layers[l_i] = abs(abs(d[3] * j + d[2] * (i))-start)
+          l_i += 1
+          start = None
+          if l_i == 3:
+            break
+      full_row_vals.append(layers)
+    directionLayers.append(full_row_vals);
 
-  print full_row_vals
+  for full_row_vals in directionLayers:
+    vals = [np.mean(full_row_vals[i:i+8], axis=0) for i in range(0,48, 8)]
+    print vals
 
-  vals = [np.mean(full_row_vals[i:i+8], axis=0) for i in range(0,48, 8)]
-  print vals
 
 
 

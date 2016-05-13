954435b8502eae046e350868ba4adacc988731b0
Sloane Sturzenegger <sloanes@stanford.edu>
2016-05-13 14:57:10 -0700

    image_gen

--- image_gen.py
+++ image_gen.py
@@ -1,7 +1,16 @@
+# -*- coding: utf-8 -*-
+
 import subprocess
+import os
 
 KANJI = "kanji_only.txt"
-FONT = "Gulim"
+FONT_FULL="'/Users/Sloane/Downloads/mplus-TESTFLIGHT-061/mplus-2p-regular.ttf'"
+FONT_SHORT = "MPlusRegular"
+
+directory = "data/kanji-%s" % FONT_SHORT
+
+if not os.path.exists(directory):
+  os.makedirs(directory)
 
 with open(KANJI, 'r') as f:
   idx = 0
@@ -11,6 +20,6 @@ with open(KANJI, 'r') as f:
     idx += 1
     cmd ="""
       convert -font %s -size 256x -background white -fill black label:"%s" data/kanji-%s/kanji_%s.png
-    """ % (FONT, text, FONT, idx)
+    """ % (FONT_FULL, text, FONT_SHORT, idx)
     print cmd 
     subprocess.call(cmd, shell=True)
